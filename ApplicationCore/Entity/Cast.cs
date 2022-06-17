6c312443933fe758dc2f2d9cee3d71c14835eb36
Jayking3413 <99855635+Jayking3413@users.noreply.github.com>
2022-06-17 14:39:38 -0500

    Add project files.

---
+++ ApplicationCore/Entity/Cast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApplicationCore.Entity
+{
+    [Table("Cast")]
+    public class Cast
+    {
+        public int Id { get; set; }
+        [MaxLength(128)]
+        public string? Name { get; set; }
+        public string? Gender { get; set; }
+        public string? TmdbUrl { get; set; }
+        public string? Profilepath { get; set; }
+
+    }
+}
---
+++ Infrastructure/Migrations/20220617180557_CreatingCastTable.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class CreatingCastTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TrailerUrl",
+                table: "Trailers",
+                type: "nvarchar(2084)",
+                maxLength: 2084,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2084)",
+                oldMaxLength: 2084);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Trailers",
+                type: "nvarchar(2084)",
+                maxLength: 2084,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2084)",
+                oldMaxLength: 2084);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genre",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TmdbUrl",
+                table: "Crew",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePath",
+                table: "Crew",
+                type: "nvarchar(2084)",
+                maxLength: 2084,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2084)",
+                oldMaxLength: 2084);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Crew",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Crew",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Cast",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TmdbUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Profilepath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cast", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cast");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TrailerUrl",
+                table: "Trailers",
+                type: "nvarchar(2084)",
+                maxLength: 2084,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2084)",
+                oldMaxLength: 2084,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Trailers",
+                type: "nvarchar(2084)",
+                maxLength: 2084,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2084)",
+                oldMaxLength: 2084,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genre",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TmdbUrl",
+                table: "Crew",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePath",
+                table: "Crew",
+                type: "nvarchar(2084)",
+                maxLength: 2084,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2084)",
+                oldMaxLength: 2084,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Crew",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Crew",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
